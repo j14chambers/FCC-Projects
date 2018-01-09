507520a9d3de2b3f960a6c930b42e639ef50f052
j14chambers <jessica14chambers@gmail.com>
2018-01-08 20:04:06 -0600

    Change JS and CSS

--- Wikipedia Viewer/wikipediaviewer.js
+++ Wikipedia Viewer/wikipediaviewer.js
@@ -5,48 +5,59 @@ $(function(){
 //do not delete above this line
     
     $('#search-click').click(function(){ 
+    	$('#wikipedia-title').html('');
     	var searchItem = document.getElementById('input-search').value;
-    	console.log(searchItem);
-    	console.log('clicked');
-    	//wikiInfo(searchItem);
+    	//console.log(searchItem);
+    	//console.log('clicked');
 
-    	$.getJSON("https://en.wikipedia.org/w/api.php?action=query&format=json&gsrlimit=15&generator=search&origin=*&gsrsearch=" + searchItem, 
-    		function(data){
-                    $('#wiki-searched').append('<h2>Top 15 Wiki Search Results for "' + searchItem + '"</h2>');
-                    $.each(data.query.pages, function (i) {
-                        $('#wikipedia-links').append("<p><a href='https://en.wikipedia.org/?curid=" + data.query.pages[i].pageid + 
-                            "' target='_blank'>" + data.query.pages[i].title + "</a></p>");
-                    });
-                    
-                });
+    	wikiInfo(searchItem);
 
     });
 
     $("#input-search").keypress(function(enter){
+    	$('#wikipedia-title').html('');
 	    if (enter.which === 13){
 	    	var searchItem = document.getElementById('input-search').value;
-	    	console.log(searchItem);
-	    	console.log('keypress')
-	    	//wikiInfo(searchItem);
-	    	}
+	    	//console.log(searchItem);
+	    	//console.log('keypress');
+
+	    	wikiInfo(searchItem);
+	    }
 	});
 
 
-    /*function wikiInfo(wikiSearchItem){
-		$.getJSON("https://en.wikipedia.org/w/api.php?action=query&format=json&gsrlimit=15&generator=search&origin=*&gsrsearch=" + wikiSearchItem ,
-			function(results){
-				$('#wikipedia-links').html(results.query);
-				console.log(results);
-				console.log('finished searching');
-				console.log('I search for ' + wikiSearchItem);
-			}
+    function wikiInfo(wikiSearchItem){
+		var url = "https://en.wikipedia.org/w/api.php?&action=opensearch&search=" + wikiSearchItem +"&format=json&callback=?";
 
-		);
-	}*/
+    	$.ajax({
+    		type:"GET",
+    		url: url,
+    		async: false,
+    		dataType: "jsonp",
+    		success: function(data){
+    			//console.log(data);
 
+    			var j = 0;
 
+    			while (j !== 10){
 
+		    	/*for(var i = 1; i < data.length; i++){
+					var dataJ = data[i];
+		    		//console.log(i);
+		    		console.log(dataJ[j]);
+		    		
+		    	}*/
+		    	$('#wikipedia-title').append("<li><a href="+data[3][j]+" target=" + "blank" + "><h2>" +data[1][j]+"</h2></a><p>"+data[2][j]+"</p></li>");
+	    		j++;
+	    		//console.log(j);
+	    	}
 
+    		},
+    		error: function(error){
+    			console.log('Error. Cannot Load Data.');
+    		}
 
+    	});
+	}
 //do not delete below this line
 });
