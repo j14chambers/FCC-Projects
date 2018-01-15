c5ce84a33aa167d085ffa48b5397865adbcb405a
j14chambers <jessica14chambers@gmail.com>
2018-01-15 15:00:29 -0600

    Update JS and html

--- Twitchtv/twitchtv.js
+++ Twitchtv/twitchtv.js
@@ -1,13 +1,7 @@
 $(function(){
 
-/*var userName1 = 'esl_sc';
-var userName2 = 'freecodecamp';
-var userName3 = 'RobotCalab';*/
-
-neebsgaming();
-//esl_sc();
-//freecodecamp();
-//RobotCalab();
+freecodecamp();
+//liveChannel();*/
 
 function url(userName){
 	$.getJSON('https://wind-bow.gomix.me/twitch-api/streams/'+ userName + '?callback=?', 
@@ -25,18 +19,15 @@ function url(userName){
 
   		status(streamStatus);
 
-  		//var description = data.stream.channel.status;
-  		//console.log(description);
-
-  		//console.log(streamReturn);
-
   	});
 
 }
 
+
 //Status Function
 function status(streamReturn){
 	console.log(streamReturn);
+	//$('.status').html('');
 
 	if(streamReturn === null){
 		console.log('Offline');
@@ -45,26 +36,43 @@ function status(streamReturn){
 		$('.nostream').html('<p>No Live Stream</p>');
 	}
 	else{
-		console.log('Online');
+		//console.log('Online');
 		var userNameReturn = streamReturn.channel.display_name;
 		var statusReturn = streamReturn.channel.status;
 		console.log(userNameReturn);
+		console.log(statusReturn);
+
+		$('.nostream').addClass('online');
 		$('.status').html('<h3> Online</h3>');
-		$('#'+ userNameReturn).append('<p>' + statusReturn + '</p>');
+		$('.online').append('<p>' + statusReturn + '</p>');
 	}
 }
+
 
 function freecodecamp(){
 	url('freecodecamp');
 }
-function esl_sc(){
-	url('esl_sc');
-}
-function RobotCalab(){
-	url('RobotCalab');
-}
-function neebsgaming(){
-	url('neebsgaming');
-}
+
+$('#live-channel').click(function liveChannel(){
+
+	$.ajax({url: 'https://api.twitch.tv/kraken/streams',
+	type: 'GET', 
+	success: function(data) {
+  		console.log(data);
+
+  		/*var url = data._links.self;
+  		console.log(url);
+
+  		var channel = data._links.channel;
+  		console.log(channel);
+
+  		var streamStatus = data.stream;
+  		console.log(streamStatus);
+
+  		status(streamStatus);*/
+
+  	}
+  });
+});
 
 });
