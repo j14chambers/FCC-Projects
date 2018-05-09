589d36f54e8fa8ee503e8aeb4965f8072840df62
j14chambers <jessica14chambers@gmail.com>
2018-05-09 12:03:00 -0500

    calculator css and js modified

---
+++ JS Calculator/calulator.js
@@ -0,0 +1,44 @@
+
+function calculator(expression){
+    console.log(expression);
+    for(var i in expression){
+        //console.log(expression[i]);
+        if(expression[i] == operation(expression[i])){
+            operationToPerform = operation(expression[i]);
+            console.log(operationToPerform);
+            var answer = expression[i-1] + expresssion[i + 1];
+            console.log(expression[i-1] +'    ' + expresssion[i + 1]);
+        }
+        else{
+
+        }
+    }
+    console.log(answer);
+
+    function operation(operator) {
+        switch(operator){
+            case '+':
+            console.log('add');
+            return ;
+            break;
+            case '-':
+            console.log('subtract');
+            break;
+            case 'x':
+            console.log('multiply');
+            break;
+            case '/':
+            console.log('divide');
+            break;
+            case '=':
+            console.log('equal');
+            break;
+            default:
+            console.log('No operation perfrom');
+        }
+    }
+}
+
+calculator('12-85+51=');
+//calculator('-1/8x58-65=');
+
