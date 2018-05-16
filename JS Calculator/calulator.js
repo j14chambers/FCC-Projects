2c59b637bb96a4de882bbd7a91de3cb707fb3771
j14chambers <jessica14chambers@gmail.com>
2018-05-15 19:34:54 -0500

    Add function evaluateExpression and add function calculate

--- JS Calculator/calulator.js
+++ JS Calculator/calulator.js
@@ -1,30 +1,37 @@
 
 $('.btn').click(function(){ 
     btnPressed = $(this).text();
-    console.log('clicked');
-    console.log(btnPressed);
+    //console.log('clicked');
+    //console.log(btnPressed);
 
     inputField(btnPressed);
+    
 });
+
 //array of btn that were clicked
 var storedClicked = [];
 
 function inputField(btnPressed){
-    console.log('input field: ' + btnPressed);
+    //console.log('input field: ' + btnPressed);
 
     //puts the btn clicked into the class input-field; see html file
-    $('.input-field').html(btnPressed);
+    $('.input-field').append(btnPressed);
+
     //pushes the btn pressed into array ' storedclicked'
     storedClicked.push(btnPressed);
     console.log(storedClicked);
 
     endOfExpression(btnPressed);
 }
+
 //if 'C' or '=' are pressed this function is ran
 function endOfExpression(){
     if(btnPressed == 'C'){
+
         //this clears the storedclicked when pressed
         storedClicked = [];
+        $('.input-field').html('');
+
     }
     else if(btnPressed == '='){
 
@@ -33,50 +40,66 @@ function endOfExpression(){
     }
 }
 
-function evaluateExpression (){
-
-}
-
-/* function calculator(expression){
+function evaluateExpression (expression){
     console.log(expression);
-    for(var i in expression){
-        //console.log(expression[i]);
-        if(expression[i] == operation(expression[i])){
-            operationToPerform = operation(expression[i]);
-            console.log(operationToPerform);
-            var answer = expression[i-1] + expresssion[i + 1];
-            console.log(expression[i-1] +'    ' + expresssion[i + 1]);
-        }
-        else{
+    var num1 = '';
+    var num2 = '';
+    var expressionSymbol = ['-', '+','x', '/'];
+    var equals = '=';
 
-        }
-    }
-    console.log(answer);
+    for (var i in expression) {
+        var currentValue = expression[i];
 
-    function operation(operator) {
-        switch(operator){
-            case '+':
-            console.log('add');
-            return ;
-            break;
-            case '-':
-            console.log('subtract');
-            break;
-            case 'x':
-            console.log('multiply');
-            break;
-            case '/':
-            console.log('divide');
-            break;
-            case '=':
-            console.log('equal');
-            break;
-            default:
-            console.log('No operation perfrom');
+        for(var e in expressionSymbol){
+            if(currentValue == expressionSymbol[e]){
+                var evaluate = currentValue;
+                //console.log(evaluate);
+
+                //console.log(i);
+                
+                num1 = expression.splice(0,i).join('');
+                //console.log(expression.length);
+            }
         }
+        if(currentValue == equals){
+            num2 = expression.splice(1,i-1).join('');
+            calculate(num1, evaluate, num2);
+            $('.input-field').append(answer);
+        }
+        
     }
+    //console.log(expression);
+    console.log('num1 = ' + num1);
+    console.log('num2 = ' + num2);
+    
+
 }
 
-calculator('12-85+51=');
-//calculator('-1/8x58-65='); */
+function calculate(num1,operator,num2) {
+    switch(operator){
+        case '+':
+        answer = num1 + num2;
+        console.log('Answer = ' + answer);
+        return answer;
+        break;
+        case '-':
+        answer = num1 - num2;
+        console.log('Answer = ' + answer);
+        return answer;
+        break;
+        case 'x':
+        answer = num1 * num2;
+        console.log('Answer = ' + answer);
+        return answer;
+        break;
+        case '/':
+        answer = num1 / num2;
+        console.log('Answer = ' + answer);        
+        return answer;
+        break;
+        default:
+        console.log('No operation perfrom');
+    }
+   
+}
 
