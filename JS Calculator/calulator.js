d0524370aa5ac63295287b7608d1f5c9c2888c87
j14chambers <jessica14chambers@gmail.com>
2018-05-22 02:14:30 -0500

    Add in functions clear(), add(), checkForOperator(); function checkForOperator works as should;

--- JS Calculator/calulator.js
+++ JS Calculator/calulator.js
@@ -1,3 +1,10 @@
+//array of btn that were clicked
+var storedClicked = [];
+var numbers = [];
+var finalNumArr = '';
+var array = [];
+var n = 0;
+
 
 $('.btn').click(function(){ 
     btnPressed = $(this).text();
@@ -8,9 +15,6 @@ $('.btn').click(function(){
     
 });
 
-//array of btn that were clicked
-var storedClicked = [];
-
 function inputField(btnPressed){
     //console.log('input field: ' + btnPressed);
 
@@ -19,11 +23,10 @@ function inputField(btnPressed){
 
     //pushes the btn pressed into array ' storedclicked'
     storedClicked.push(btnPressed);
-    console.log(storedClicked);
+    //console.log(storedClicked);
 
     endOfExpression(btnPressed);
 }
-
 //if 'C' or '=' are pressed this function is ran
 function endOfExpression(){
     if(btnPressed == 'C'){
@@ -36,11 +39,70 @@ function endOfExpression(){
     else if(btnPressed == '='){
 
         //if equals is pressed evaluate expression is ran
-        evaluateExpression(storedClicked);
+        //evaluateExpression(storedClicked);
+        checkForOperator(storedClicked);
     }
 }
 
-function evaluateExpression (expression){
+//Check for operator position and splice expression 
+//Finding the first number
+function checkForOperator(expression){
+    console.log(expression);
+    var expressionSymbol = ['-', '+','x', '/'];
+    var integer = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    var equals = '=';
+    
+        for(var e in expression){
+            console.log(expression[e]);
+
+            console.log('Integer indexof: ' + integer.indexOf(expression[e]));
+
+            if(integer.indexOf(expression[e]) !== -1){
+                numbers.push(Number(expression[e])); 
+            }
+            console.log('exprssionsymbol indexof: ' + expressionSymbol.indexOf(expression[e]));
+            if(expressionSymbol.indexOf(expression[e]) !== -1){
+                operator = expression[e];
+                console.log('operator: ' + operator);
+                
+                break;
+            }
+            numbers.join('');
+            console.log('numbers ');
+            console.log(numbers.join(''));
+            finalNum = numbers.join('');
+        }
+        numbers = [];
+        console.log(finalNum);
+
+        expression.splice(0,e);
+        expression.splice(0,1);
+        console.log(expression);
+
+       storeNumbers(finalNum, n);
+       n++;
+        while(expression.length > 0){
+            checkForOperator(expression);
+        }
+
+    }
+    function storeNumbers(num, length){
+
+        console.log('Stored Numbers in Array ' + length);
+
+        console.log(num);
+
+        array[length] = (Number(num));
+        console.log(array[length]);
+            
+        //var array = [];
+        //array.push(Number(num));
+        console.log(array);
+    }
+   
+
+
+function evaluateExpression(expression){
     console.log(expression);
     var num1 = '';
     var num2 = '';
@@ -59,47 +121,35 @@ function evaluateExpression (expression){
                 
                 num1 = expression.splice(0,i).join('');
                 //console.log(expression.length);
+                console.log(expression);
+
+                num3 = expression.splice(1,i).join('');
+                console.log(num3);
+                console.log(expression);
             }
         }
-        if(currentValue == equals){
-            num2 = expression.splice(1,i-1).join('');
-            calculate(num1, evaluate, num2);
-            $('.input-field').append(answer);
-        }
+        // if(currentValue == equals){
+        //     num2 = expression.splice(1,i-1).join('');
+        //     //calculate(num1, evaluate, num2);
+        //     //$('.input-field').append(answer);
+        // }
         
     }
     //console.log(expression);
     console.log('num1 = ' + num1);
-    console.log('num2 = ' + num2);
-    
-
+    console.log('num2 = ' + num3);
 }
 
-function calculate(num1,operator,num2) {
-    switch(operator){
-        case '+':
-        answer = num1 + num2;
-        console.log('Answer = ' + answer);
-        return answer;
-        break;
-        case '-':
-        answer = num1 - num2;
-        console.log('Answer = ' + answer);
-        return answer;
-        break;
-        case 'x':
-        answer = num1 * num2;
-        console.log('Answer = ' + answer);
-        return answer;
-        break;
-        case '/':
-        answer = num1 / num2;
-        console.log('Answer = ' + answer);        
-        return answer;
-        break;
-        default:
-        console.log('No operation perfrom');
-    }
-   
-}
+function add(){
 
+}
+var clearData = document.getElementsByClassName("clear");
+clearData.addEventListener("click",clear());
+
+function clear(){
+    var storedClicked = [];
+    var numbers = [];
+    var finalNumArr = '';
+    var array = [];
+    var n = 0;
+}
